50e46ff33185843b48213ff578c6844d93d57913
Yangye Zhu <yangye.zhu@wishlife.com>
2021-11-10 13:36:15 -0800

    tweak manage script

--- manage.sh
+++ manage.sh
@@ -5,19 +5,18 @@ argument=$1
 if [ $argument = "up" ]; then
     echo "Creating infrastructure..."
     docker-compose up -d
-    docker-compose -f superset/superset-service.yml up
+    docker-compose -f superset/superset-service.yml up -d
 elif [ $argument = "stop" ]; then
     echo "Stopping infrastructure..."
     docker-compose stop
 elif [ $argument = "down" ]; then
     echo "Deleting infrastructure..."
-    docker-compose down
+    docker-compose down -v
 elif [ $argument = "restart" ]; then
     echo "Restarting infrastructure..."
-    docker-compose down
-    docker-compose build
+    docker-compose down -v
     docker-compose up -d
-    docker-compose -f superset/superset-service.yml up
+    docker-compose -f superset/superset-service.yml up -d
 else
   echo "Unknown argumnet! Options: up, stop, down, restart"
 fi
