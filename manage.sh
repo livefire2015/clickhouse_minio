da86108c2d78e5e1bc757812e81a0fe75db8f351
Yangye Zhu <yangye.zhu@wishlife.com>
2021-11-10 12:28:04 -0800

    tweak manage: start superset

--- manage.sh
+++ manage.sh
@@ -5,6 +5,7 @@ argument=$1
 if [ $argument = "up" ]; then
     echo "Creating infrastructure..."
     docker-compose up -d
+    docker-compose -f superset/superset-service.yml up
 elif [ $argument = "stop" ]; then
     echo "Stopping infrastructure..."
     docker-compose stop
@@ -16,6 +17,7 @@ elif [ $argument = "restart" ]; then
     docker-compose down
     docker-compose build
     docker-compose up -d
+    docker-compose -f superset/superset-service.yml up
 else
   echo "Unknown argumnet! Options: up, stop, down, restart"
 fi
