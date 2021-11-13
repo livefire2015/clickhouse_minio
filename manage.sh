4bd760b21bb1a34928f049182d86c1e80fa2afb4
Yangye Zhu <yangye.zhu@wishlife.com>
2021-11-13 09:16:49 -0800

    fix: manage script for superset

--- manage.sh
+++ manage.sh
@@ -9,12 +9,15 @@ if [ $argument = "up" ]; then
 elif [ $argument = "stop" ]; then
     echo "Stopping infrastructure..."
     docker-compose stop
+    docker-compose -f superset/superset-service.yml stop
 elif [ $argument = "down" ]; then
     echo "Deleting infrastructure..."
     docker-compose down -v
+    docker-compose -f superset/superset-service.yml down -v
 elif [ $argument = "restart" ]; then
     echo "Restarting infrastructure..."
     docker-compose down -v
+    docker-compose -f superset/superset-service.yml down -v
     docker-compose up -d
     docker-compose -f superset/superset-service.yml up -d
 else
